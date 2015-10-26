f360d3560d6ac3a7eda13206d14d14d353b781ff
Josh Kadis <josh@alleyinteractive.com>
2015-10-26 09:56:44 -0700

    recompile scaffold

---
+++ inc/fields.php
@@ -0,0 +1,60 @@
+<?php
+
+/* begin fm:sponsorship-campaign-info */
+/**
+ * `sponsorship-campaign-info` Fieldmanager fields.
+ */
+function sponsorship_manager_fm_sponsorship_campaign_info() {
+	$fm = new Fieldmanager_Group( array(
+		'name' => 'sponsorship-campaign-info',
+		'children' => array(
+			'sponsorship-campaign' => new Fieldmanager_Select( array(
+				'label' => __( 'Select Campaign', 'sponsorship-manager' ),
+				'first_empty' => true,
+				'datasource' => new Fieldmanager_Datasource_Term( array(
+					'taxonomy' => 'sponsorship_campaign',
+					'taxonomy_hierarchical' => true,
+					'taxonomy_hierarchical_depth' => 2,
+					'taxonomy_save_to_terms' => true,
+					'only_save_to_taxonomy' => true,
+				) ),
+			) ),
+			'dfp-tracking-pixel' => new Fieldmanager_Link( array(
+				'label' => __( 'DFP Tracking Pixel URL', 'sponsorship-manager' ),
+				'description' => __( "Include 'c' parameter; cache busting will be handled on output", 'sponsorship-manager' ),
+			) ),
+			'hide-from-recent-posts' => new Fieldmanager_Checkbox( array(
+				'label' => __( 'Hide from Recent Posts queries', 'sponsorship-manager' ),
+				'checked_value' => true,
+				'default_value' => true,
+			) ),
+			'hide-from-feeds' => new Fieldmanager_Checkbox( array(
+				'label' => __( 'Hide from feed queries (RSS, etc.)', 'sponsorship-manager' ),
+				'checked_value' => true,
+				'default_value' => true,
+			) ),
+		),
+	) );
+	$fm->add_meta_box( __( 'Sponsorship Campaign', 'sponsorship-manager' ), array( 'post' ) );
+}
+add_action( 'fm_post_post', 'sponsorship_manager_fm_sponsorship_campaign_info' );
+/* end fm:sponsorship-campaign-info */
+
+/* begin fm:tax_sponsorship_campaign:sponsorship-campaign-display */
+/**
+ * `sponsorship-campaign-display` Fieldmanager fields.
+ */
+function sponsorship_manager_fm_tax_sponsorship_campaign_sponsorship_campaign_display() {
+	$fm = new Fieldmanager_Group( array(
+		'name' => 'sponsorship-campaign-display',
+		'children' => array(
+			'logo' => new Fieldmanager_Media( __( 'Campaign Logo', 'sponsorship-manager' ) ),
+			'featured-image' => new Fieldmanager_Media( __( 'Campaign Featured Image', 'sponsorship-manager' ) ),
+			'external-url' => new Fieldmanager_Link( __( 'Campaign External URL', 'sponsorship-manager' ) ),
+			'description' => new Fieldmanager_RichTextArea( __( 'Campaign Description', 'sponsorship-manager' ) ),
+		),
+	) );
+	$fm->add_term_form( __( 'Display Fields', 'sponsorship-manager' ), array( 'sponsorship_campaign' ) );
+}
+add_action( 'fm_term_sponsorship_campaign', 'sponsorship_manager_fm_tax_sponsorship_campaign_sponsorship_campaign_display' );
+/* end fm:tax_sponsorship_campaign:sponsorship-campaign-display */
---
+++ inc/taxonomies/class-sponsorship_manager-taxonomy-sponsorship-campaign.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Taxonomy for Sponsorship Campaigns.
+ */
+class Sponsorship_Manager_Taxonomy_Sponsorship_Campaign extends Sponsorship_Manager_Taxonomy {
+
+	/**
+	 * Name of the taxonomy.
+	 *
+	 * @var string
+	 */
+	public $name = 'sponsorship_campaign';
+
+	/**
+	 * Object types for this taxonomy
+	 *
+	 * @var array
+	 */
+	public $object_types;
+
+
+	/**
+	 * Build the taxonomy object.
+	 */
+	public function __construct() {
+		$this->object_types = array( 'empty' );
+
+		parent::__construct();
+	}
+
+	/**
+	 * Creates the taxonomy.
+	 */
+	public function create_taxonomy() {
+		register_taxonomy( $this->name, $this->object_types, array(
+			'labels' => array(
+				'name'                  => __( 'Sponsorship Campaigns', 'sponsorship-manager' ),
+				'singular_name'         => __( 'Sponsorship Campaign', 'sponsorship-manager' ),
+				'search_items'          => __( 'Search Sponsorship Campaigns', 'sponsorship-manager' ),
+				'popular_items'         => __( 'Popular Sponsorship Campaigns', 'sponsorship-manager' ),
+				'all_items'             => __( 'All Sponsorship Campaigns', 'sponsorship-manager' ),
+				'parent_item'           => __( 'Parent Sponsorship Campaign', 'sponsorship-manager' ),
+				'parent_item_colon'     => __( 'Parent Sponsorship Campaign', 'sponsorship-manager' ),
+				'edit_item'             => __( 'Edit Sponsorship Campaign', 'sponsorship-manager' ),
+				'view_item'             => __( 'View Sponsorship Campaign', 'sponsorship-manager' ),
+				'update_item'           => __( 'Update Sponsorship Campaign', 'sponsorship-manager' ),
+				'add_new_item'          => __( 'Add New Sponsorship Campaign', 'sponsorship-manager' ),
+				'new_item_name'         => __( 'New Sponsorship Campaign Name', 'sponsorship-manager' ),
+				'add_or_remove_items'   => __( 'Add or remove Sponsorship Campaigns', 'sponsorship-manager' ),
+				'choose_from_most_used' => __( 'Choose from most used Sponsorship Campaigns', 'sponsorship-manager' ),
+				'menu_name'             => __( 'Sponsorship Campaigns', 'sponsorship-manager' ),
+			),
+			'hierarchical' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => false,
+			'show_tagcloud' => false,
+			'show_in_quick_edit' => true,
+			'meta_box_cb' => false,
+			'rewrite' => array(
+				'slug' => 'sponsor',
+			),
+		) );
+	}
+}
+
+$taxonomy_sponsorship_campaign = new Sponsorship_Manager_Taxonomy_sponsorship_campaign();
