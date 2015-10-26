171f8dbdc70148b1516197219701b735e9c2e356
Josh Kadis <josh@alleyinteractive.com>
2015-10-26 09:53:40 -0700

    reset scaffold

--- functions.php
+++ functions.php
@@ -5,49 +5,4 @@
 
 /* Begin data structures */
 
-// Taxonomy Base Class
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy.php' );
-
-// Sponsorship Campaigns Taxonomy (tax:sponsorship-campaign)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign.php' );
-
-// Fieldmanager Fields
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/fields.php' );
-
-// Fieldmanager Fields
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/fields.php' );
-
-// Taxonomy Base Class
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy.php' );
-
-// Sponsorship Campaigns Taxonomy (tax:sponsorship_campaign)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign.php' );
-
-// Fieldmanager Fields
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/fields.php' );
-
-// Taxonomy Base Class
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy.php' );
-
-// Sponsorship Campaigns Taxonomy (tax:sponsorship_campaign)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign.php' );
-
-// Taxonomy Base Class
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy.php' );
-
-// Sponsorship Campaigns Taxonomy (tax:sponsorship_campaign)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign.php' );
-
-// Fieldmanager Fields
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/fields.php' );
-
-// Taxonomy Base Class
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy.php' );
-
-// Sponsorship Campaigns Taxonomy (tax:sponsorship_campaign)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign.php' );
-
-// Sponsorship Campaign Posts Taxonomy (tax:sponsorship_campaign_posts)
-require_once( SPONSORSHIP_MANAGER_PATH . '/inc/taxonomies/class-sponsorship-manager-taxonomy-sponsorship-campaign-posts.php' );
-
 /* End data structures */
