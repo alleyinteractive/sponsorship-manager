a6ba2bc7ba2201e0c067ce95fa6d1d98de0b473c
Josh Kadis <josh@alleyinteractive.com>
2015-10-28 14:27:37 -0700

    just the changes to archiveless

--- inc/modules/class-sponsorship-archiveless.php
+++ inc/modules/class-sponsorship-archiveless.php
@@ -1,27 +1,8 @@
 <?php
 
-/*
-	Plugin Name: Archiveless
-	Plugin URI: https://github.com/alleyinteractive/archiveless
-	Description: Hide posts from archives performantly
-	Version: 0.1
-	Author: Alley Interactive
-	Author URI: http://www.alleyinteractive.com/
-*/
-/*  This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/**
+ * Adapted from https://github.com/alleyinteractive/archiveless
+ */
 
 class Archiveless {
 
@@ -29,7 +10,9 @@ class Archiveless {
 
 	public $status = 'archiveless';
 
-	protected $meta_key = 'archiveless';
+	protected $meta_key = 'sponsorship-info';
+
+	protected $archiveless_meta_key = 'archiveless';
 
 	private function __construct() {
 		/* Don't do anything, needs to be initialized via instance() method */
@@ -49,13 +32,10 @@ class Archiveless {
 	public function setup() {
 		add_action( 'init', array( $this, 'register_post_status' ) );
 		add_filter( 'wp_insert_post_data', array( $this, 'wp_insert_post_data' ), 10, 2 );
-		add_action( 'save_post', array( $this, 'save_post' ) );
 
 		if ( is_admin() ) {
-			add_action( 'post_submitbox_misc_actions', array( $this, 'add_ui' ) );
 			add_action( 'add_meta_boxes', array( $this, 'fool_edit_form' ) );
 		} else {
-			// add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
 			add_filter( 'posts_where', array( $this, 'posts_where' ), 10, 2 );
 		}
 	}
@@ -81,20 +61,6 @@ class Archiveless {
 	}
 
 	/**
-	 * Add the checkbox to the post edit screen to give the option to hide a
-	 * post from archives.
-	 */
-	public function add_ui() {
-		global $post;
-		?>
-		<div class="misc-pub-section">
-			<input type="hidden" name="<?php echo esc_attr( $this->meta_key ) ?>" value="0" />
-			<label><input type="checkbox" name="<?php echo esc_attr( $this->meta_key ) ?>" value="1" <?php checked( '1', get_post_meta( $post->ID, $this->meta_key, true ) ) ?> /> <?php esc_html_e( 'Hide from Archives', 'archiveless' ) ?></label>
-		</div>
-		<?php
-	}
-
-	/**
 	 * Set the custom post status when post data is being inserted.
 	 *
 	 * WordPress, unfortunately, doesn't provide a great way to _manage_ custom
@@ -108,28 +74,19 @@ class Archiveless {
 	 * @return array $data, potentially with a new status.
 	 */
 	public function wp_insert_post_data( $data, $postarr ) {
+		// use custom status when published post has "hide from..." box checked`
+		error_log($data['post_status']);
 		if ( 'publish' == $data['post_status'] ) {
-			if ( isset( $_POST[ $this->meta_key ] ) ) {
-				if ( '1' === $_POST[ $this->meta_key ] ) {
+			if ( ! empty( $_POST[ $this->meta_key ][ $this->archiveless_meta_key ] ) && '1' === $_POST[ $this->meta_key ][ $this->archiveless_meta_key ] ) {
+				$data['post_status'] = $this->status;
+			} elseif ( ! empty( $postarr['ID'] ) ) {
+				if ( ! empty( $postarr[ $this->meta_key ][ $this->archiveless_meta_key ] ) && '1' === $postarr[ $this->meta_key ][ $this->archiveless_meta_key ] ) {
 					$data['post_status'] = $this->status;
 				}
-			} elseif ( ! empty( $postarr['ID'] ) && '1' === get_post_meta( $postarr['ID'], $this->meta_key, true ) ) {
-				$data['post_status'] = $this->status;
 			}
 		}
-
+		error_log($data['post_status'] . "\n");
 		return $data;
-	}
-
-	/**
-	 * Store the value of the "Hide form Archives" checkbox to post meta.
-	 *
-	 * @param  int $post_id Post ID.
-	 */
-	public function save_post( $post_id ) {
-		if ( isset( $_POST[ $this->meta_key ] ) ) {
-			update_post_meta( $post_id, $this->meta_key, intval( $_POST[ $this->meta_key ] ) );
-		}
 	}
 
 	/**
@@ -145,7 +102,9 @@ class Archiveless {
 	}
 
 	/**
-	 * Hide archiveless posts on non-singular pages.
+	 * Hide archiveless posts from applicable queries.
+	 * We do this by _removing_ the archiveless status
+	 * when it's not wanted, becaues WP defaults to querying all public statuses
 	 *
 	 * @param  string $where MySQL WHERE clause.
 	 * @param  WP_Query $query Current WP_Query object.
@@ -154,11 +113,37 @@ class Archiveless {
 	 */
 	public function posts_where( $where, $query ) {
 		global $wpdb;
-		if ( $query->is_main_query() && ! $query->is_singular() && false !== strpos( $where, " OR {$wpdb->posts}.post_status = '{$this->status}'" ) ) {
+
+		// make sure archiveless status is there in the first place
+		if ( false === strpos( $where, " OR {$wpdb->posts}.post_status = '{$this->status}'" ) ) {
+			return $where;
+		}
+
+		// default to hiding archiveless status
+		$hide_archiveless = true;
+
+		// except when NOT a main query or a feed
+		if ( ! $query->is_main_query() && ! $query->is_feed() ) {
+			$hide_archiveless = false;
+		}
+		// or when main query for singular or campaign archive
+		elseif ( $query->is_main_query() && ( $query->is_singular() || $query->is_tax( SPONSORSHIP_MANAGER_CAMPAIGN_TAXONOMY ) ) ) {
+			$hide_archiveless = false;
+		}
+
+		// anything else you can think of
+		if ( apply_filters( 'sponsorship_manager_show_archiveless', false, $query ) ) {
+			$hide_archiveless = false;
+		}
+
+		// remove archiveless status from SQL query
+		if ( $hide_archiveless ) {
 			$where = str_replace( " OR {$wpdb->posts}.post_status = '{$this->status}'", '', $where );
 		}
 
 		return $where;
 	}
 }
-add_action( 'after_setup_theme', array( 'Archiveless', 'instance' ) );
+
+// go go go
+Archiveless::instance();
