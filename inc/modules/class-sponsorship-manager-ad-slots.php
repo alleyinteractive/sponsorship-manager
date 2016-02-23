b9198608d2f01a45d67d79ed17e99863ac48c391
Josh Kadis <josh@alleyinteractive.com>
2016-02-22 22:09:37 -0500

    php5.3 compat for empty()

--- inc/modules/class-sponsorship-manager-ad-slots.php
+++ inc/modules/class-sponsorship-manager-ad-slots.php
@@ -448,7 +448,8 @@ class Sponsorship_Manager_Ad_Slots {
 	 */
 	public function do_api_request( $query ) {
 		// must be main query and our API query var
-		if ( ! $query->is_main_query() || empty( $slot_name = $query->get( 'sponsorship_ad_slot') ) ) {
+		$slot_name = $query->get( 'sponsorship_ad_slot');
+		if ( ! $query->is_main_query() || empty( $slot_name ) ) {
 			return;
 		}
 
@@ -532,7 +533,8 @@ add_action( 'init', 'sponsorship_manager_setup_ad_slots', 11 );
  * @return array List of IDs
  */
 function sponsorship_manager_has_eligible_posts ( $slot_name, $args = false ) {
-	return ! empty( Sponsorship_Manager_Ad_Slots::instance()->get_eligible_posts( $slot_name, $args ) );
+	$posts = Sponsorship_Manager_Ad_Slots::instance()->get_eligible_posts( $slot_name, $args );
+	return ! empty( $posts );
 }
 
 /**
